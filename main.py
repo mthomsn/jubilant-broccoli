6f7202ef8cbc587beebc27ea3efc1b0d8e6ac89f
matt <matthewthomsen99@gmail.com>
2023-10-19 00:08:35 -0500

    add function to get future/past fixtures.
    start working on cron automation

--- main.py
+++ main.py
@@ -14,22 +14,32 @@ with open('ids.json') as json_file:
     ids = json.load(json_file)
     print(ids['laliga_2023_id'])
 
-# function to test sending text message
 # assuming carrier will be verizon for simplicity
 def send_txt(number):
     print(f'{number}@vtext.com')
-    ezgmail.send(f'{number}@vtext.com', '', 'this is a test of python script')
+    ezgmail.send(f'{number}@vtext.com', '', 'this is a test of python script using cron')
 
-# function to call api and recieve football data
-def get_future_fixtures(league_id, season, from_date, to_date):
+def get_league_fixtures(league_id, season, from_date, to_date):
     url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
     querystring = {"league":f"{league_id}","season":f"{season}","from":f"{from_date}","to":f"{to_date}"}
-    headers = {
-	"X-RapidAPI-Key": RAPID_API_KEY,
-	"X-RapidAPI-Host": RAPID_API_HOST
-}
+    headers = {"X-RapidAPI-Key": RAPID_API_KEY, "X-RapidAPI-Host": RAPID_API_HOST}
     response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
 
-    print(response.json())
+# returns fixture on given time, future or past fixtures indicated by future param
+def get_fixture(team_id, season, qty, future):
+    if future == 'next':
+        querystring = {"team":f"{team_id}", "season":f"{season}", "next":f"{qty}"}
+    elif future == 'last':
+        querystring = {"team":f"{team_id}", "season":f"{season}", "last":f"{qty}"}
+    elif future != 'next' and future != 'last':
+        print("invalid future/past indicator")
+        exit()
+    url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
+    headers = {"X-RapidAPI-Key": RAPID_API_KEY, "X-RapidAPI-Host": RAPID_API_HOST}
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
 
-get_future_fixtures(ids['laliga_2023_id'], "2023", "2023-10-21", "2023-10-23")
+#fixtures = get_league_fixtures(ids['laliga_2023_id'], "2023", "2023-10-21", "2023-10-23")
+fixtures = get_fixture(ids['barca_team_id'], 2023, 1, 'last')
+print(json.dumps(fixtures, indent=4))
